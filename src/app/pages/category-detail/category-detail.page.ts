e1ab89ad3f59672981236d8c72baf58234b25628
erdoClasital <erdogan@clasital.com>
2021-11-03 20:30:38 +0300

    back button

--- src/app/pages/category-detail/category-detail.page.ts
+++ src/app/pages/category-detail/category-detail.page.ts
@@ -1,65 +1,78 @@
-import { Component, NgZone, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CategoryListModel, PostApiService, PostListModel } from 'src/app/services/api.service';
-import { AppService } from 'src/app/services/app.service';
-import { Browser } from '@capacitor/browser';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CategoryListModel, PostApiService, PostListModel} from 'src/app/services/api.service';
+import {AppService} from 'src/app/services/app.service';
+import {Browser} from '@capacitor/browser';
+import {Location} from '@angular/common';
+import {NavController} from "@ionic/angular";
 
 @Component({
-  selector: 'app-category-detail',
-  templateUrl: './category-detail.page.html',
-  styleUrls: ['./category-detail.page.scss'],
+    selector: 'app-category-detail',
+    templateUrl: './category-detail.page.html',
+    styleUrls: ['./category-detail.page.scss'],
 })
 export class CategoryDetailPage implements OnInit {
 
-  posts: PostListModel[];
-  category: CategoryListModel;
+    posts: PostListModel[];
+    category: CategoryListModel;
 
-  constructor(
-    private zone: NgZone,
-    private router: Router,
-    private route: ActivatedRoute,
-    private appService: AppService,
-    private postApiService: PostApiService
-  ) { }
-
-  ngOnInit() {
-    this.route.queryParams.subscribe(v => {
-      const categoryId = parseInt(v.categoryId);
-      this.category = this.appService.userCategories.find(x => x.id == categoryId);
-    });
-
-    if (this.category != null) {
-      this.loadPosts();
+    constructor(
+        private zone: NgZone,
+        private router: Router,
+        private route: ActivatedRoute,
+        private appService: AppService,
+        private postApiService: PostApiService,
+        private location: Location,
+        private navController:NavController
+    ) {
     }
-  }
 
-  loadPosts() {
-    this.postApiService.category(this.category.id)
-      .subscribe(
-        v => this.onPostsLoad(v),
-        e => this.onError(e)
-      )
-  }
-  onPostsLoad(v: PostListModel[]): void {
-    this.zone.run(() => {
-      this.posts = v;
-    })
-  }
-  onError(e: any): void {
-    this.posts = [];
-    this.appService.showErrorAlert(e);
-  }
+    ngOnInit() {
+        this.route.queryParams.subscribe(v => {
+            const categoryId = parseInt(v.categoryId);
+            this.category = this.appService.userCategories.find(x => x.id == categoryId);
 
-  async redirectToUrl(url: string) {
-    await Browser.open({ url: url });
-  }
+        });
 
-  addProduct() {
-    this.router.navigate(['/add-product/' + this.category.id]);
-  }
+        if (this.category != null) {
+            this.loadPosts();
+        }
+    }
 
-  openEditCategory() {
-    this.router.navigate(["/tabs/home/add-category"], { queryParams: { categoryId: this.category.id } });
-  }
+    back() {
+        this.navController.setDirection("back", true, "back");
+        this.location.back()
+    }
+
+    loadPosts() {
+        this.postApiService.category(this.category.id)
+            .subscribe(
+                v => this.onPostsLoad(v),
+                e => this.onError(e)
+            )
+    }
+
+    onPostsLoad(v: PostListModel[]): void {
+        this.zone.run(() => {
+            this.posts = v;
+        })
+    }
+
+    onError(e: any): void {
+        this.posts = [];
+        this.appService.showErrorAlert(e);
+    }
+
+    async redirectToUrl(url: string) {
+        await Browser.open({url: url});
+    }
+
+    addProduct() {
+        this.router.navigate(['/add-product/' + this.category.id]);
+    }
+
+    openEditCategory() {
+        this.router.navigate(["/tabs/home/add-category"], {queryParams: {categoryId: this.category.id}});
+    }
 
 }
