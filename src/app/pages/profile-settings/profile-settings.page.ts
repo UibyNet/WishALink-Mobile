075d57a5c3bc60813c6919b357809bdaa59ca8d4
erdoClasital <erdogan@clasital.com>
2021-11-03 22:01:48 +0300

    profile update

--- src/app/pages/profile-settings/profile-settings.page.ts
+++ src/app/pages/profile-settings/profile-settings.page.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
 import {AppService} from "../../services/app.service";
-import {SocialUserListModel} from "../../services/api.service";
+import {ProfileApiService, UserModel} from "../../services/api.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-profile-settings',
@@ -10,14 +12,71 @@ import {SocialUserListModel} from "../../services/api.service";
 export class ProfileSettingsPage implements OnInit {
 
     constructor(
-        private appService: AppService
+        private profileApiService: ProfileApiService,
+        private appService: AppService,
+        private zone: NgZone,
+        private formBuilder: FormBuilder,
+        private router: Router
     ) {
     }
 
-    userData: SocialUserListModel
+    userData: UserModel
+    userProfilePic: string
+    updateForm: FormGroup
+    isLoading: boolean = false;
 
     ngOnInit() {
-        this.userData = this.appService.userInfo
+        this.getUserInfo()
+        this.userProfilePic = this.appService.userInfo.profilePictureUrl
+        this.updateForm = this.formBuilder.group({
+            firstName: [this.userData?.firstName, [Validators.required, Validators.minLength(1)]],
+            lastName: [this.userData?.lastName, [Validators.required, Validators.minLength(1)]],
+            email: [this.userData?.email, [Validators.required, Validators.minLength(1), Validators.email]],
+        })
     }
 
+    getUserInfo() {
+        this.profileApiService.detail().subscribe(
+            v => this.onUserData(v),
+            e => this.onError(e)
+        )
+    }
+
+    onUserData(v: UserModel) {
+        this.zone.run(() => {
+            this.userData = v
+            this.updateForm.get('firstName').setValue(v.firstName)
+            this.updateForm.get('lastName').setValue(v.lastName)
+            this.updateForm.get('email').setValue(v.email)
+        })
+    }
+
+    onError(e: any) {
+        this.isLoading = false
+        this.zone.run(() => {
+            this.appService.showAlert(e)
+        })
+    }
+
+    updateUserData() {
+        const model = new UserModel();
+        model.firstName = this.updateForm.get('firstName').value.trim();
+        model.lastName = this.updateForm.get('lastName').value.trim();
+        model.email = this.updateForm.get('email').value.trim();
+
+        this.isLoading = true;
+        this.profileApiService.update(model).subscribe(
+            v => this.onUpdate(v),
+            e => this.onError(e)
+        )
+    }
+
+    onUpdate(v: void) {
+        this.zone.run(() => {
+            this.appService.userInfo.fullName = this.updateForm.get('firstName').value.trim() + ' ' + this.updateForm.get('lastName').value.trim()
+            this.isLoading = false
+            this.appService.showToast('Profil başarıyla güncellendi')
+            this.router.navigate(['/tabs/settings'])
+        })
+    }
 }
