9744e48f0d2564598691c08686cddaf44ccc6ce1
erdoClasital <erdogan@clasital.com>
2021-11-13 17:40:56 +0200

    Oneriler sayfasi

--- src/app/pages/tabs/suggestion/suggestion.page.ts
+++ src/app/pages/tabs/suggestion/suggestion.page.ts
@@ -1,10 +1,10 @@
-import { Component, NgZone, OnInit } from '@angular/core';
-import { PostApiService, PostListModel, SocialUserListModel } from 'src/app/services/api.service';
-import { AppService } from "../../../services/app.service";
-import { Browser } from '@capacitor/browser';
-import { ModalController } from '@ionic/angular';
-import { NotificationComponent } from 'src/app/components/notification/notification.component';
-import { Router } from '@angular/router';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {PostApiService, PostListModel, SocialUserListModel} from 'src/app/services/api.service';
+import {AppService} from "../../../services/app.service";
+import {Browser} from '@capacitor/browser';
+import {ModalController} from '@ionic/angular';
+import {NotificationComponent} from 'src/app/components/notification/notification.component';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-suggestion',
@@ -24,11 +24,13 @@ export class SuggestionPage implements OnInit {
         private modalController: ModalController
     ) {
     }
+
     ngOnInit() {
         this.userData = this.appService.userInfo;
 
         this.loadSuggestions(null)
     }
+
     ionViewWillEnter() {
         this.appService.toggleStatusBar('light');
 
@@ -47,18 +49,20 @@ export class SuggestionPage implements OnInit {
                 }
             )
     }
+
     onSuggestionsLoad(v: PostListModel[]): void {
         this.zone.run(() => {
             if (this.suggestions == null) this.suggestions = [];
             this.suggestions = this.suggestions.concat(v);
         })
     }
+
     onError(e: any): void {
         this.appService.showErrorAlert(e);
     }
 
     async redirectToUrl(url: string) {
-        await Browser.open({ url: url });
+        await Browser.open({url: url});
     }
 
     async openNotification() {
@@ -74,10 +78,19 @@ export class SuggestionPage implements OnInit {
         return await modal.present();
     }
 
-    openPostDetail(postId: number) {
+    openPostDetail(event: any, postId: number) {
+        console.log(event.target.classList)
         const post = this.suggestions.find(x => x.id == postId);
         if (post != null) {
-            this.router.navigate(['/tabs/post-detail'], { state: post, queryParams: { isStrangerPost: true } });
+            if (event.target.tagName == 'IMG') {
+                console.log(post)
+                this.redirectToUrl(post.url)
+            } else {
+                this.router.navigate(['/tabs/post-detail'], {state: post, queryParams: {isStrangerPost: true}});
+            }
         }
+
+        event.preventDefault();
+        return false
     }
 }
