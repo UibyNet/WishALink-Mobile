615eb7a8fcf1431735a2eec0a321c5f05ffc4f62
yakupkocaman <63709446+yakupkocaman@users.noreply.github.com>
2021-11-23 22:32:09 +0200

    Cüzdan ayarları eklendi

--- src/app/pages/tabs/wallet/wallet.page.ts
+++ src/app/pages/tabs/wallet/wallet.page.ts
@@ -22,6 +22,10 @@ export class WalletPage {
     private paymentRequestApiService: KpayFixedqrPaymentPayment_RequestApiService
   ) { }
 
+  ionViewDidEnter() {
+    this.startPayment();
+  }
+
   openCards() {
     this.router.navigateByUrl(`/app/wallet/cards`);
   }
